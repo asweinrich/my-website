c07cf736d0a1bc25bcaca21a7d137e1799fc575f
Andrew Weinrich <asweinrich@gmail.com>
2024-09-25 13:04:43 -0700

    updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,16 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+const werdna = localFont({
+  src: [
+    {
+      path: './fonts/Werdna-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    }
+  ],
+})
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${werdna.className} antialiased`}
       >
         {children}
       </body>
